1d53abbc985d24e5b4420d38fa9a61c0daf89a39
Mishraco <shwetamishra0904@gmail.com>
2024-07-30 12:40:07 -0400

    Adding New sqls for July30

--- Leetcode50/sql0730.sql
+++ Leetcode50/sql0730.sql
@@ -10,3 +10,32 @@ join total_entries t on q1.query_name=t.query_name
 join queries q2
 on q1.query_name=q2.query_name and q1.result=q2.result
 group by q1.query_name
+
+--OR
+
+select
+query_name,
+round(avg(cast(rating as decimal) / position), 2) as quality,
+round(sum(case when rating < 3 then 1 else 0 end) * 100 / count(*), 2) as poor_query_percentage
+from
+queries
+group by
+query_name;
+
+
+--20
+
+# Write your MySQL query statement below
+
+select LEFT(trans_date,7) as month,
+       country,
+       count(*) as trans_count,
+       sum(case when state='approved' then 1 else 0 end) as approved_count,
+       sum(amount) as trans_total_amount,
+       sum(case when state='approved' then amount else 0 end) as approved_total_amount
+from Transactions
+group by 1,2     
+
+--date_format(trans_date, "%Y-%m") as month
+--EXTRACT(Year_month from trans_date)
+--SUBSTR(trans_date,1,7)
